f6d2d0d494fa0710e113ca3c884dcd333e8bb015
Luke Gibson <ldgibson@uw.edu>
2018-05-17 14:57:03 -0700

    updated structure to follow a class design

--- npia/core.py
+++ npia/core.py
@@ -1,19 +1,29 @@
-from .preparation import *
-from .processing import *
-from .analysis import *
+import mahotas as mh
+import numpy as np
 
+class AFMImage:
+    def __init__(self, image_file, scale_bar=None, ub=None, lb=None):
+        self.image = cv2.imread(image_file)
+        self.original_image = None
+        self.scale_bar = scale_bar
+        self.upper_bound = ub
+        self.lower_bound = lb
+        
+        self.n_particles = None
+        self.particle_distances = []  # (x, y, radius, nearest_neighbor_dist)
+        return
 
-def load_and_prepare():
-    return
+    def prepare(self):
+        return
 
+    def apply_threshold(self):
+        return
 
-def apply_threshold():
-    return
+    def watershed(self):
+        return
 
+    def get_distances(self):
+        return
 
-def get_distances():
-    return
-
-
-def show_image():
-    return
+    def show_image(self):
+        return
