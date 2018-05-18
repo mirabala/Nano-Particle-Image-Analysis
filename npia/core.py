49226a01f0267d87f83282442b2363fcd8f27881
Damilola Daramola <kaizenstatus@gmail.com>
2018-05-17 18:28:47 -0700

    adding the image pre-processing section to the core file

--- npia/core.py
+++ npia/core.py
@@ -11,13 +11,24 @@ class Image:
         self.scale_bar = scale_bar
         self.upper_bound = ub
         self.lower_bound = lb
-        
+
+	self.labels = None
+	self.pix_to_micron = None
         self.n_particles = None
         self.particle_distances = []  # (x, y, radius, nearest_neighbor_dist)
         return
 
     # Dami
-    def prepare(self):
+
+    def rgb2gray(self):
+        return np.dot(self[...,:3], [0.299, 0.587, 0.114])
+
+    def prepare(self, ipx=2015, ipy=2015):		# ipx & ipy are exported pixel counts from AFM
+        self.image = rgb2gray(self)			#
+        self.image = self.image[2:ipy,2:ipx] 		# Crop image
+	scal = self.image[2030,0:2000]			#
+	bar = np.count_nonzero(scal-255)		#
+	self.pix_to_micron = self.scale_bar/bar 	# pixel to um scale conversion
         return
 
     # Luke
